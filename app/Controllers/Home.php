9665d689f69d6df32d00afe8ed3831c855b43058
Dominique Foley <dfoley@bestcigarprices.com>
2023-04-30 20:38:23 -0400

    Footer created for global components

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('layout/global_header') . view('page/index') . view('layout/global_footer'); 
     }
 }
